b99e85b2fadc620a3dbda380fa8f4793f4953289
lky <597572246@qq.com>
2023-05-18 17:00:15 +0000

    feat: finish sending arp-request

--- include/arp.h
+++ include/arp.h
@@ -1,6 +1,23 @@
 #ifndef ARP_H
 #define ARP_H
 
+#include <linux/if_arp.h>
 
+// ETH_HLEN + sizeof(arp_header)
+#define PACARP_LEN 42
+
+struct arp_header {
+    __be16 ar_hrd;  /* format of hardware address */
+    __be16 ar_pro;  /* format of protocol address */
+    unsigned char ar_hln;  /* length of hardware address */
+    unsigned char ar_pln;  /* length of protocol address */
+    __be16 ar_op;  /* ARP opcode (command) */
+    unsigned char ar_sha[ETH_ALEN]; /* sender hardware address */
+    unsigned char ar_sip[4];  /* sender IP address  */
+    unsigned char ar_tha[ETH_ALEN]; /* target hardware address */
+    unsigned char ar_tip[4];  /* target IP address  */
+};
+
+int build_request();
 
 #endif
--- src/arp.c
+++ src/arp.c
@@ -2,3 +2,69 @@
 #include "args.h"
 #include "utils.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <linux/if_ether.h>
+#include <linux/if_arp.h>
+#include <arpa/inet.h>
+
+uint8_t bcast_mac[ETH_ALEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+void build_packet(char *ef) {
+    struct ethhdr *p_ethhdr;
+    struct arp_header *p_arphdr;
+    // set ether header
+    p_ethhdr = (struct ethhdr *)ef;
+    memcpy(p_ethhdr->h_source, local_mac, ETH_ALEN);
+    memcpy(p_ethhdr->h_dest, bcast_mac, ETH_ALEN);
+    p_ethhdr->h_proto = htons(ETH_P_ARP);
+    // set arp header
+    p_arphdr = (struct arp_header *)(ef + ETH_HLEN);
+    p_arphdr->ar_hrd = htons(ARPHRD_ETHER);
+    p_arphdr->ar_pro = htons(ETH_P_IP); // ipv4
+    p_arphdr->ar_hln = ETH_ALEN;
+    p_arphdr->ar_pln = sizeof(struct in_addr);
+    p_arphdr->ar_op = htons(ARPOP_REQUEST);
+    memcpy(p_arphdr->ar_sha, local_mac, ETH_ALEN);
+    memcpy(p_arphdr->ar_sip, &local_ip, sizeof(struct in_addr));
+    memcpy(p_arphdr->ar_tha, bcast_mac, ETH_ALEN);
+    memcpy(p_arphdr->ar_tip, &target, sizeof(struct in_addr));
+}
+
+void init_sockaddr(struct sockaddr_ll *dev) {
+    memset(dev, 0, sizeof(struct sockaddr_ll));
+    dev->sll_family = AF_PACKET; /* Always AF_PACKET */
+    dev->sll_protocol = htons(ETH_P_ARP); /* Physical-layer protocol */
+    dev->sll_ifindex = ifindex; /* Interface number */
+    dev->sll_hatype = ARPHRD_ETHER; /* ARP hardware type */
+    dev->sll_pkttype = PACKET_BROADCAST;  /* Packet type */
+    dev->sll_halen = ETH_ALEN; /* Length of address */
+    memcpy(dev->sll_addr, local_mac, ETH_ALEN); /* Physical-layer address */
+}
+
+int build_request() {
+    static char ef[PACARP_LEN];
+    int i, e, s;
+    struct sockaddr_ll dev;
+    s = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
+    if (s < 0) {
+        perror(RED "Init socket");
+        return -1;
+    }
+    puts(BLUE "Building ARP-Request packet..." END);
+    build_packet(ef);
+    init_sockaddr(&dev);
+    for (i = 0; i < times; i++) {
+        e = sendto(s, ef, PACARP_LEN, 0, (struct sockaddr *)&dev, sizeof(dev));
+        if (e < 0) {
+            perror(RED "Send packet");
+            close(s);
+            return -1;
+        }
+        printf(BLUE "Sending #%d...\n" END, i);
+        sleep(1);
+    }
+    return close(s), 0;
+}
